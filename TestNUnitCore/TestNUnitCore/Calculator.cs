12bd9ccd66b40773fd45e8ab7f50eced27d85109
ppalasz <40823206+ppalasz@users.noreply.github.com>
2018-07-26 15:49:03 +0200

    Add files via upload

---
+++ TestNUnitCore/TestNUnitCore/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TestNUnitCore
+{
+    public class Calculator
+    {
+        public static string Multiply(float arg1, float arg2)
+        {
+            return (arg1 * arg2).ToString();
+        }
+
+        public static string Divide(float arg1, float arg2)
+        {
+            return (arg1 % arg2).ToString();
+        }
+
+        public static string Adding(float arg1, float arg2)
+        {
+            return (arg1 + arg2).ToString();
+        }
+
+        public static string Substract(float arg1, float arg2)
+        {
+            Thread.Sleep(4000);
+            return (arg1 - arg2).ToString();
+        }
+    }
+}
---
+++ TestNUnitCore/XUnitTest/NUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TestNUnitCore;
+
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class NUnitTest
+    {
+        [Test]
+        public void TestCalculatorAdding()
+        {
+            string sol = Calculator.Adding(10, 20);
+            Assert.AreEqual("30", sol);
+        }
+
+        [Test]
+        [MaxTime(4000)]
+        public void TestCalculatorSubstract()
+        {
+
+            string sol = Calculator.Substract(50, 50);
+            Assert.AreEqual("0", sol);
+        }
+
+        [Test]
+        public void TestCalculatorMultiply(
+            [Random(int.MinValue, int.MaxValue, 100)] float x)
+        {
+            string sol = Calculator.Multiply(x, 0);
+            Assert.AreEqual("0", sol);
+        }
+
+        [TestCase(14, 4, "3.5")]
+        [TestCase(16, 4, "4")]
+        [TestCase(25, 5, "5")]
+        public void TestCalculatorDivide(float x, float y, string s)
+        {
+            string sol = Calculator.Divide(x, y);
+            Assert.AreEqual(s, sol);
+        }
+    }
+}
